0b2f31026615296cea728983a528cc4095062990
navneet <navneetpalgithub@gmail.com>
2024-05-21 12:25:47 +0530

    Sup Jupiter

--- app/src/main/java/com/example/tic_tac_toe/GameScreen.kt
+++ app/src/main/java/com/example/tic_tac_toe/GameScreen.kt
@@ -5,11 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tic_tac_toe.ui.theme.BlueCustom
 import com.example.tic_tac_toe.ui.theme.GrayBackground
 
 
@@ -24,9 +30,32 @@ fun GameScreen(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ){
-        Row {
-
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Player '0': 0",
+                fontSize = 16.sp
+            )
+            Text(
+                text = "Draw: 0",
+                fontSize = 16.sp
+            )
+            Text(
+                text = "Player 'X': 0",
+                fontSize = 16.sp
+            )
         }
+        Text(
+            text = "Tic Tac Toe",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            color = BlueCustom
+        )
     }
 
 }
